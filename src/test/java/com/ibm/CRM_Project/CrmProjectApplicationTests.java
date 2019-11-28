27e98b5bc563f63b2ba93b8219c7d6fa0ead3194
GHenique <guillaume.henique@ibm.com>
2019-11-28 13:32:26 +0100

    first commit Spring Boot Template

---
+++ src/test/java/com/ibm/CRM_Project/CrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.CRM_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
